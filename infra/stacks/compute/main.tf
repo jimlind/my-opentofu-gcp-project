42f43133c2b33b7f14b91d7efff738b3ef3638c4
Jim Lind <spoon.vw@gmail.com>
2025-06-24 18:18:24 -0400

    remove disks from compute state

--- infra/stacks/compute/main.tf
+++ infra/stacks/compute/main.tf
@@ -4,19 +4,8 @@ provider "google" {
   zone    = "us-central1-a"
 }
 
-resource "google_compute_disk" "disks" {
-  for_each = toset(["disk-a", "disk-b"])
-
-  name  = each.key
-  type  = "pd-standard"
-  zone  = "us-central1-a"
-  size  = 10
-}
-
 resource "google_compute_instance" "instances" {
-  for_each = toset(["vm-a", "vm-b"])
-
-  name         = each.key
+  name         = "vm-a"
   machine_type = "e2-micro"
   zone         = "us-central1-a"
 
